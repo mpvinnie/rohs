1e724e23bf78a84c234aafa34e1b5c524ebee6d6
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-02-03 10:24:14 -0400

    add tests to delete subpart

---
+++ server/src/modules/parts/useCases/deleteSubpart/DeleteSubpartUseCase.spec.ts
@@ -0,0 +1,229 @@
+import { FakePartsRepository } from '@modules/parts/repositories/fakes/FakePartsRepository'
+import { FakeSubpartsRepository } from '@modules/parts/repositories/fakes/FakeSubpartsRepository'
+import { FakeProvidersRepository } from '@modules/providers/repositories/fakes/FakeProvidersRepository'
+import { FakeStorageProvider } from '@shared/containers/providers/StorageProvider/fakes/FakeStorageProvider'
+import { AppError } from '@shared/errors/AppError'
+
+import { DeleteSubpartUseCase } from './DeleteSubpartUseCase'
+
+let providersRepository: FakeProvidersRepository
+let partsRepository: FakePartsRepository
+let subpartsRepository: FakeSubpartsRepository
+let storageProvider: FakeStorageProvider
+let deleteSubpart: DeleteSubpartUseCase
+
+describe('DeleteSubpart', () => {
+  beforeEach(() => {
+    providersRepository = new FakeProvidersRepository()
+    partsRepository = new FakePartsRepository()
+    subpartsRepository = new FakeSubpartsRepository()
+    storageProvider = new FakeStorageProvider()
+
+    deleteSubpart = new DeleteSubpartUseCase(
+      providersRepository,
+      partsRepository,
+      subpartsRepository,
+      storageProvider
+    )
+  })
+
+  it('should be able to delete a subpart', async () => {
+    const provider = await providersRepository.create({
+      id: '12345678',
+      name: 'Provider Name',
+      cnpj: '12345679801234',
+      password: 'password',
+      segment: 'Segment'
+    })
+
+    const part = await partsRepository.create({
+      provider_id: provider.id,
+      code: '123456',
+      description: 'Part Description',
+      comment: 'Comment'
+    })
+
+    const subpart = await subpartsRepository.create({
+      provider_id: provider.id,
+      part_id: part.id,
+      name: 'Subpart Name',
+      gwi_11a1: 'gwi_11a1.doc',
+      fisp_msds: 'fisp.doc',
+      rohs_report: 'rohs.doc',
+      subgroup: 'subgroup'
+    })
+
+    await deleteSubpart.execute({
+      provider_id: provider.id,
+      part_id: part.id,
+      subpart_id: subpart.id
+    })
+
+    const deletedSubpart = await subpartsRepository.findByPartId(
+      part.id,
+      subpart.id
+    )
+
+    expect(deletedSubpart).toBeUndefined()
+  })
+
+  it('should not be able to delete a non-existent provider`s subpart', async () => {
+    const part = await partsRepository.create({
+      provider_id: 'provider-id',
+      code: '123456',
+      description: 'Part Description',
+      comment: 'Comment'
+    })
+
+    const subpart = await subpartsRepository.create({
+      provider_id: 'provider-id',
+      part_id: part.id,
+      name: 'Subpart Name',
+      gwi_11a1: 'gwi_11a1.doc',
+      fisp_msds: 'fisp.doc',
+      rohs_report: 'rohs.doc',
+      subgroup: 'subgroup'
+    })
+
+    await expect(
+      deleteSubpart.execute({
+        provider_id: 'non-existent-provider-id',
+        part_id: part.id,
+        subpart_id: subpart.id
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to delete a non-existent part`s subpart', async () => {
+    const provider = await providersRepository.create({
+      id: '12345678',
+      name: 'Provider Name',
+      cnpj: '12345679801234',
+      password: 'password',
+      segment: 'Segment'
+    })
+
+    const subpart = await subpartsRepository.create({
+      provider_id: provider.id,
+      part_id: 'part_id',
+      name: 'Subpart Name',
+      gwi_11a1: 'gwi_11a1.doc',
+      fisp_msds: 'fisp.doc',
+      rohs_report: 'rohs.doc',
+      subgroup: 'subgroup'
+    })
+
+    await expect(
+      deleteSubpart.execute({
+        provider_id: provider.id,
+        part_id: 'non-existent-part-id',
+        subpart_id: subpart.id
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  // it('should not be able to delete a subpart if part is currently approved', async () => {
+  //   const provider = await providersRepository.create({
+  //     id: '12345678',
+  //     name: 'Provider Name',
+  //     cnpj: '12345679801234',
+  //     password: 'password',
+  //     segment: 'Segment'
+  //   })
+
+  //   const part = await partsRepository.create({
+  //     provider_id: provider.id,
+  //     code: '123456',
+  //     description: 'Part Description',
+  //     comment: 'Comment'
+  //   })
+
+  //   const subpart = await subpartsRepository.create({
+  //     provider_id: provider.id,
+  //     part_id: part.id,
+  //     name: 'Subpart Name',
+  //     gwi_11a1: 'gwi_11a1.doc',
+  //     fisp_msds: 'fisp.doc',
+  //     rohs_report: 'rohs.doc',
+  //     subgroup: 'subgroup'
+  //   })
+
+  //   await deleteSubpart.execute({
+  //     provider_id: provider.id,
+  //     part_id: part.id,
+  //     subpart_id: subpart.id
+  //   })
+
+  //   const deletedSubpart = await subpartsRepository.findByPartId(
+  //     part.id,
+  //     subpart.id
+  //   )
+
+  //   expect(deletedSubpart).toBeUndefined()
+  // })
+
+  // it('should not be able to delete a subpart if part is currently under analysis', async () => {
+  //   const provider = await providersRepository.create({
+  //     id: '12345678',
+  //     name: 'Provider Name',
+  //     cnpj: '12345679801234',
+  //     password: 'password',
+  //     segment: 'Segment'
+  //   })
+
+  //   const part = await partsRepository.create({
+  //     provider_id: provider.id,
+  //     code: '123456',
+  //     description: 'Part Description',
+  //     comment: 'Comment'
+  //   })
+
+  //   const subpart = await subpartsRepository.create({
+  //     provider_id: provider.id,
+  //     part_id: part.id,
+  //     name: 'Subpart Name',
+  //     gwi_11a1: 'gwi_11a1.doc',
+  //     fisp_msds: 'fisp.doc',
+  //     rohs_report: 'rohs.doc',
+  //     subgroup: 'subgroup'
+  //   })
+
+  //   await deleteSubpart.execute({
+  //     provider_id: provider.id,
+  //     part_id: part.id,
+  //     subpart_id: subpart.id
+  //   })
+
+  //   const deletedSubpart = await subpartsRepository.findByPartId(
+  //     part.id,
+  //     subpart.id
+  //   )
+
+  //   expect(deletedSubpart).toBeUndefined()
+  // })
+
+  it('should be able to delete a non-existent subpart', async () => {
+    const provider = await providersRepository.create({
+      id: '12345678',
+      name: 'Provider Name',
+      cnpj: '12345679801234',
+      password: 'password',
+      segment: 'Segment'
+    })
+
+    const part = await partsRepository.create({
+      provider_id: provider.id,
+      code: '123456',
+      description: 'Part Description',
+      comment: 'Comment'
+    })
+
+    await expect(
+      deleteSubpart.execute({
+        provider_id: provider.id,
+        part_id: part.id,
+        subpart_id: 'non-existent-subpart'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
