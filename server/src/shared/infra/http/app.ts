8c6882cb3d6f13d4ea33dcea61bf5f28544d1fd8
Vinicius Peres <vinniemalafaia@hotmail.com>
2022-02-12 15:07:35 -0400

    configuring socket io

---
+++ server/src/shared/infra/http/app.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata'
+import 'dotenv/config'
+import cors from 'cors'
+import express from 'express'
+import 'express-async-errors'
+import http from 'http'
+import { Server } from 'socket.io'
+
+import '@shared/infra/prisma'
+import '@shared/containers'
+import uploadConfig from '@config/upload'
+import { asyncErrors } from '@shared/errors/asyncErrors'
+
+import { appRoutes } from './routes'
+
+const app = express()
+
+app.use(cors())
+
+export const serverHttp = http.createServer(app)
+
+export const io = new Server(serverHttp, {
+  cors: {
+    origin: '*'
+  }
+})
+
+io.on('connection', socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
+
+app.use(express.json())
+
+app.use('/files/avatars', express.static(uploadConfig.uploadFolders.avatars))
+
+app.use(
+  '/files/documents',
+  express.static(uploadConfig.uploadFolders.documents)
+)
+
+app.use('/files/subparts', express.static(uploadConfig.uploadFolders.subparts))
+
+app.use(appRoutes)
+
+app.use(asyncErrors)
--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -1,35 +1,5 @@
-import 'reflect-metadata'
-import 'dotenv/config'
-import cors from 'cors'
-import express from 'express'
-import 'express-async-errors'
+import { serverHttp } from './app'
 
-import '@shared/infra/prisma'
-import '@shared/containers'
-import uploadConfig from '@config/upload'
-import { asyncErrors } from '@shared/errors/asyncErrors'
-
-import { appRoutes } from './routes'
-
-const app = express()
-
-app.use(express.json())
-
-app.use(cors())
-
-app.use('/files/avatars', express.static(uploadConfig.uploadFolders.avatars))
-
-app.use(
-  '/files/documents',
-  express.static(uploadConfig.uploadFolders.documents)
-)
-
-app.use('/files/subparts', express.static(uploadConfig.uploadFolders.subparts))
-
-app.use(appRoutes)
-
-app.use(asyncErrors)
-
-app.listen(3333, () => {
+serverHttp.listen(3333, () => {
   console.log('Server running on port 3333')
 })
