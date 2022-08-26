04946f45da5ed562259b791e67fd67f76b05b8ca
Gabriel-biel <gabriel97gla98@gmail.com>
2022-08-26 18:54:58 -0400

    update: :construction_worker: :hammer: new-parts

--- front-provider/src/pages/Provider/NewPart/index.tsx
+++ front-provider/src/pages/Provider/NewPart/index.tsx
@@ -1,3 +1,32 @@
+import addPart from '../../../assets/defaultAvatar.png'
+import logoImg from '../../../assets/logo.svg'
+import { FormButton } from '../../../components/FormButton'
+import { Container, Content, Form, Header } from './styles'
+
 export function NewPart() {
-  return <h1>NewPart</h1>
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Rohs" />
+        <Form>
+          <Header>
+            <h3>Detalhes da Peça</h3>
+            <input type="text" placeholder="Salcomp Code. *" />
+            <input type="text" placeholder="Description . *" />
+            <p>
+              Preenchimento GW114-11A1 <br />
+              Prezado(a) muita atenção no preenchimento do formulário de
+              Declaração de Material GW114-11A1 <br />
+              ...(importância do documento ...faça do download do formulário{' '}
+              <a>clicando aqui</a> preencha e registre ao lado
+            </p>
+            <img src={addPart} alt="Update image" />
+          </Header>
+          <FormButton isSubmitting={false} title={'Enviar Para Aprovação'} />
+          <FormButton isSubmitting={false} title={'Salvar Temporariamente'} />
+          <FormButton isSubmitting={false} title={'Cancelar'} />
+        </Form>
+      </Content>
+    </Container>
+  )
 }
--- front-provider/src/pages/Provider/NewPart/styles.ts
+++ front-provider/src/pages/Provider/NewPart/styles.ts
@@ -1,104 +1,72 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  display: grid;
-  grid-template-areas: 'sidebar main';
-  grid-template-columns: minmax(15rem, 15rem) 1fr;
-
-  @media (max-width: 1040px) {
-    grid-template-columns: minmax(6rem, 6rem) 1fr;
-  }
-
-  height: 100vh;
-`
-
-export const Main = styled.main`
-  grid-area: main;
-
+  background: var(--green_900);
   width: 100%;
 
-  background: var(--gray_50);
-
   display: flex;
-  flex-direction: column;
   align-items: center;
-`
-
-export const Content = styled.div`
-  width: 100%;
-  padding: 3.5rem;
-  display: flex;
   justify-content: center;
 `
 
-export const PartsContainer = styled.div`
-  background: var(--white);
-  border-radius: 1rem;
-  padding: 2rem;
-  width: 100%;
-  max-width: 70rem;
-  box-shadow: 0px 8px 18px 1px rgba(0, 0, 0, 0.25);
-
+export const Content = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 
-  header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    h3 {
-      font-size: 1.5rem;
-      color: var(--blue_900);
-    }
+  img {
+    margin: 1.5rem 0 1rem 0;
   }
 `
 
-export const Table = styled.table`
-  width: 100%;
-  margin: 2rem 0;
+export const Form = styled.form`
+  background: var(--white);
+  padding: 2rem;
+  width: 100vh;
+  border-radius: 1rem;
 
-  border-collapse: collapse;
-
-  thead {
-    tr {
-      th {
-        border-bottom: 1px solid var(--gray_100);
-        padding: 0.5rem;
-        text-align: left;
-        font-size: 0.875rem;
-        font-weight: 500;
-        color: var(--purple_300);
-      }
-    }
+  display: flex;
+  flex-direction: column;
   }
 
-  tbody {
-    tr {
-      td {
-        border-bottom: 1px solid var(--gray_100);
-        max-width: 8rem;
-        padding: 1rem 0.5rem;
-        text-align: left;
-        font-size: 0.875rem;
-        font-weight: 400;
-        color: var(--blue_900);
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-
-        button {
-          font-weight: 600;
-          color: var(--blue_500);
-          background: none;
-
-          transition: filter 0.2s;
-
-          &:hover {
-            filter: brightness(0.9);
-          }
-        }
-      }
-    }
+  button {
+    witdh: 50%;
+    margin: 10px;
   }
+`
+
+export const Header = styled.header`
+h3 {
+    margin-bottom: 1rem;
+    font-size: 23px;
+  }
+
+  input {
+    width: 50%;
+    margin: 0 1rem 1rem 0;
+  }
+
+  input:nth-child(2) {
+    width: 30%;
+  }
+
+  p {
+    width: 70%;
+  }
+
+  img {
+    width: 8rem;
+    height: 8rem;
+  }
+
+  a {
+    align-self: flex-end;
+    margin: 0.5rem 0 2.5rem;
+    font-size: 0.875rem;
+    color: var(--blue_500);
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
 `
