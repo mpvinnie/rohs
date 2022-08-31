3399483529a052bd78ba2b77f0a9473473912e97
Gabriel-biel <gabriel97gla98@gmail.com>
2022-08-31 15:33:10 -0400

    update: :wrench: :hammer: Working...

--- front-provider/src/pages/Provider/NewPart/index.tsx
+++ front-provider/src/pages/Provider/NewPart/index.tsx
@@ -1,3 +1,4 @@
+import fispqmsds from '../../../assets/fispqmsds.png'
 import addPart from '../../../assets/imgNewPart.png'
 import logoImg from '../../../assets/logo.svg'
 import { FormButton } from '../../../components/FormButton'
@@ -8,7 +9,8 @@ import {
   Form,
   Header,
   InputWrapper,
-  Details
+  Details,
+  Table
 } from './styles'
 
 export function NewPart() {
@@ -25,16 +27,50 @@ export function NewPart() {
             </InputWrapper>
             <Details>
               <p>
-                Preenchimento GW114-11A1 <br />
-                Prezado(a) muita atenção no preenchimento do formulário de
-                Declaração de Material GW114-11A1 <br />
-                ...(importância do documento ...faça do download do formulário
-                {''}
+                Preenchimento GW114-11A1 Prezado(a) muita atenção no
+                preenchimento do formulário de Declaração de Material GW114-11A1
+                ...importância do documento ...faça do download do formulário
                 <a>clicando aqui</a> preencha e registre ao lado
               </p>
               <img src={addPart} alt="Update image" />
             </Details>
           </Header>
+          <Table>
+            <thead>
+              <tr>
+                <th>Subpartes</th>
+                <th>Material_type</th>
+                <th>FISPQ-MSDS</th>
+                <th>RoHS Report</th>
+                <th>RoHS Report Date</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Subpart_Name</td>
+                <td>
+                  <select id="subparts" name="lista-subparts">
+                    <option>Metal</option>
+                    <option>Plastico</option>
+                    <option>Papel</option>
+                    <option>Fita</option>
+                  </select>
+                </td>
+                <td>
+                  <a>
+                    <img src={fispqmsds} alt="downloadarquivo" />
+                  </a>
+                </td>
+                <td>
+                  <a>
+                    <img src={fispqmsds} alt="downloadarquivo" />
+                  </a>
+                </td>
+                <td>31/08/2022</td>
+              </tr>
+            </tbody>
+          </Table>
           <FormButton isSubmitting={false} title={'Enviar Para Aprovação'} />
         </Form>
       </Content>
--- front-provider/src/pages/Provider/NewPart/styles.ts
+++ front-provider/src/pages/Provider/NewPart/styles.ts
@@ -20,10 +20,17 @@ export const Content = styled.div`
 `
 
 export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
   width: 100%;
-  background: var(--white);
+  max-width: 70rem;
   padding: 2rem;
+
+  background: var(--white);
+
   border-radius: 1rem;
+  box-shadow: 0px 8px 18px 1px rgba(0, 0, 0, 0.25);
 
   h3 {
     margin-bottom: 1rem;
@@ -32,7 +39,7 @@ export const Form = styled.form`
 
   button {
     witdh: 50%;
-    margin: 10px;
+    margin-top: 2rem;
   }
 `
 
@@ -44,6 +51,8 @@ export const Header = styled.header`
   img {
     width: 8rem;
     height: 8rem;
+
+    margin: 0 auto;
   }
 
   a {
@@ -59,22 +68,79 @@ export const Header = styled.header`
 `
 
 export const InputWrapper = styled.div`
-  label {
-    font-size: 11px;
+  display: flex;
+  margin-bottom: 2rem;
+
+  div:nth-child(1) {
+    margin-top: 1.5rem;
   }
 
-  input {
-    width: 30%;
-    height: 3rem;
-    border-radius: 10px 10px;
-  }
-
-  input:nth-child(2) {
-    width: 60%;
-    margin-left: 1rem;
+  div:nth-child(2) {
+    margin-left: 1.5rem;
   }
 `
 
 export const Details = styled.div`
   display: flex;
+`
+
+export const Table = styled.table`
+  width: 100%;
+  margin: 2rem 0;
+
+  border-collapse: collapse;
+
+  thead {
+    tr {
+      th {
+        border-bottom: 1px solid var(--gray_100);
+        padding: 0.5rem;
+        text-align: center;
+        font-size: 0.875rem;
+        font-weight: 500;
+        color: var(--purple_300);
+      }
+    }
+  }
+
+  tbody {
+    tr {
+      td {
+        border-bottom: 1px solid var(--gray_100);
+        max-width: 8rem;
+        padding: 1rem 0.5rem;
+        text-align: center;
+        font-size: 0.875rem;
+        font-weight: 400;
+        color: var(--blue_900);
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        button {
+          font-weight: 600;
+          color: var(--blue_500);
+          background: none;
+
+          transition: filter 0.2s;
+
+          &:hover {
+            filter: brightness(0.9);
+          }
+        }
+      }
+    }
+  }
+
+  a {
+    align-self: flex-end;
+    margin: 0.5rem 0 2.5rem;
+    font-size: 0.875rem;
+    color: var(--blue_500);
+    transition: filter 0.2s;
+
+    &:hover {
+      cursor: pointer;
+      filter: brightness(0.9);
+    }
 `
